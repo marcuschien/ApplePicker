cf0eb753e48d89798bf8a21e5a50ac4af932e941
marcuschien <mchien5@uwo.ca>
2017-11-06 16:18:40 -0500

    Add files via upload

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+
+    static public int score = 1000;
+
+    void Awake()
+    {
+      // If the ApplePickerHighScore already exists, read it
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        { 
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        // Assign the high score to ApplePickerHighScore
+        PlayerPrefs.SetInt("ApplePickerHighScore", score); 
+    }
+
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+
+        // Update ApplePickerHighScore in PlayerPrefs if necessary
+        if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        { 
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+
+    }
+}
